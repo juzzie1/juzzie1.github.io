6f1d4f084a6dc7687a735dea7e1d6a722c5a53cb
juzzie1 <68693024+juzzie1@users.noreply.github.com>
2022-07-24 08:53:51 +1000

    Add files via upload

---
+++ juz.js
@@ -0,0 +1,9 @@
+
+	now = new Date();
+	localtime = now.toString();
+	utctime = now.toGMTString();
+	document.write("<p><strong>Local Time:</strong> " + localtime + "</p>");
+	
+	document.write("<p><strong>UTC Time:</strong> " + utctime + "</p>");
+
+
